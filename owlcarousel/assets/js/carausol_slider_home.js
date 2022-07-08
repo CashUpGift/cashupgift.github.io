2fc51581fde12cf66c33e4c568b2558795302b8b
samsabbagh <whatshappeningsam23@gmail.com>
2022-07-08 22:59:34 +0530

    Cashupgift ui ux first commit

---
+++ owlcarousel/assets/js/carausol_slider_home.js
@@ -0,0 +1,110 @@
+	
+/*scrolling banner*/
+(function ($) {
+    "use strict";
+  
+       $(document).ready(function(){
+           $('.carousel_se_01_carousel' ).owlCarousel({
+              items: 3,
+              nav: true,
+              loop :true,
+             
+              mouseDrag: true,
+              responsiveClass: true,
+              /*navText : ["<i class='fas fa-long-arrow-alt-left'></i>","<i class='fas fa-long-arrow-alt-right'></i>"],*/
+              responsive: {
+                  0:{
+                    items: 1
+                  },
+                  480:{
+                    items: 1
+                  },
+                  767:{
+                    items: 2
+                  },
+                  992:{
+                    items: 3
+                  },
+                  1200:{
+                    items: 3
+                  }
+              }
+            });
+  
+       });
+  
+  
+  
+      $(document).ready(function(){
+          $('.carousel_se_02_carousel').owlCarousel({
+              items: 4,
+              nav: true,
+              loop :true,
+             
+              mouseDrag: true,
+              responsiveClass: true,
+              navText : ["<i class='icofont-bubble-left' style='color: #f6c23e;'></i>","<i class='icofont-bubble-right' style='color: #f6c23e;'></i>"],
+              responsive: {
+                  0:{
+                    items: 4
+                  },
+                  480:{
+                    items: 4
+                  },
+                  767:{
+                    items: 4
+                  },
+                  992:{
+                    items: 4
+                  },
+                  1200:{
+                    items: 4
+                  }
+              }
+          });
+      });  
+  
+  
+      $(document).ready(function(){
+          $('.carousel_se_03_carousel').owlCarousel({
+              items: 4,
+              nav: true,
+              dots: false,
+              loop :true,
+             
+              mouseDrag: true,
+              responsiveClass: true,
+              autoplay: true,
+              autoplayTimeout: 3000,
+              autoplayHoverPause: true,
+              navText : ["<i class='icofont-scroll-left'></i>","<i class='icofont-scroll-right'></i>"],
+              responsive: {
+                  0:{
+                    items: 1
+                  },
+                  480:{
+                    items: 2
+                  },
+                  767:{
+                    items: 3
+                  },
+                  992:{
+                    items: 3
+                  },
+                  1200:{
+                    items: 4
+                  }
+              }
+          });
+      });  
+  
+  
+      
+  
+  
+  
+        
+  
+  
+  })(jQuery); 
+  
